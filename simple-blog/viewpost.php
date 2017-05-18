4f3e22875ba2b22c86853ed4a871b8ad90d4a768
aqil4k <mdaqil4k@gmail.com>
2017-05-18 15:31:39 +0530

    added simple blog

---
+++ simple-blog/viewpost.php
@@ -0,0 +1,66 @@
+<?php require('includes/config.php'); 
+
+$stmt = $db->prepare('SELECT postID, postTitle, postCont, postDate FROM blog_posts WHERE postID = :postID');
+$stmt->execute(array(':postID' => $_GET['id']));
+$row = $stmt->fetch();
+
+//if post does not exists redirect user.
+if($row['postID'] == ''){
+	header('Location: ./');
+	exit;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Blog - <?php echo $row['postTitle'];?></title>
+    <link rel="stylesheet" href="style/normalize.css">
+    <link rel="stylesheet" href="style/main.css">
+</head>
+<body>
+
+	<div id="wrapper">
+
+		<h1>Blog</h1>
+		<hr />
+		<p><a href="./">Blog Index</a></p>
+
+
+		<?php	
+			echo '<div>';
+				echo '<h1>'.$row['postTitle'].'</h1>';
+				echo '<p>Posted on '.date('jS M Y', strtotime($row['postDate'])).'</p>';
+				echo '<p>'.$row['postCont'].'</p>';				
+			echo '</div>';
+		?>
+
+	</div>
+		
+	<div style="width: 400px; border: 2px solid;float: right;">
+
+	<?php
+		try {
+
+			$stmt = $db->query('SELECT postID, postTitle, postDesc, postDate FROM blog_posts ORDER BY postID DESC');
+			while($row = $stmt->fetch()){
+				
+				echo '<div>';
+					echo '<h1><a href="viewpost.php?id='.$row['postID'].'">'.$row['postTitle'].'</a></h1>';
+					echo '<p>Posted on '.date('jS M Y H:i:s', strtotime($row['postDate'])).'</p>';
+					echo '<p>'.$row['postDesc'].'</p>';				
+					echo '<p><a href="viewpost.php?id='.$row['postID'].'">Read More</a></p>';				
+				echo '</div>';
+
+			}
+
+		} catch(PDOException $e) {
+		    echo $e->getMessage();
+		}
+	?>
+
+		
+	</div>
+</body>
+</html>
