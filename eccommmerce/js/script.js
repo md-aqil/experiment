d7dd438e3727c7816a8a81d2e56c0b5defb96d68
md-aqil <aqilali381@gmail.com>
2018-04-18 17:40:33 +0530

    added

--- eccommmerce/js/script.js
+++ eccommmerce/js/script.js
@@ -230,7 +230,9 @@ $(document).ready(function(){
 		$("body").append('<div class="floating-cart"></div>');		
 		var cart = $('div.floating-cart');		
 		productCard.clone().appendTo(cart);
-		$(cart).css({'top' : position.top + 'px', "left" : position.left + 'px'}).fadeIn("slow").addClass('moveToCart');		
+		
+		$(cart).css({'top' : position.top + 'px', "left" : position.left + 'px'}).fadeIn("slow").addClass('moveToCart');
+
 		setTimeout(function(){$("body").addClass("MakeFloatingCart");}, 800);
 		setTimeout(function(){
 			$('div.floating-cart').remove();
